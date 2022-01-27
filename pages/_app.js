69798d38caa300225f7fdea8411b88d1e0a139c1
gaoussou <gaoussoukone@protonmail.com>
2022-01-27 22:58:13 +0100

    adding chakra ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,22 @@
-import { ApolloProvider } from '@apollo/client'
-import { useApollo } from '../lib/apolloClient'
+import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
+import { ApolloProvider } from '@apollo/client';
+import { useApollo } from '../lib/apolloClient';
+import theme from '../theme';
 
 export default function App({ Component, pageProps }) {
   const apolloClient = useApollo(pageProps)
 
   return (
-    <ApolloProvider client={apolloClient}>
-      <Component {...pageProps} />
-    </ApolloProvider>
+    <ChakraProvider resetCSS theme={theme}>
+      <ColorModeProvider
+        options={{
+          useSystemColorMode: true,
+        }}
+      >
+        <ApolloProvider client={apolloClient}>
+          <Component {...pageProps} />
+        </ApolloProvider>
+      </ColorModeProvider>
+    </ChakraProvider >
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,28 +2,25 @@ import App from '../components/App'
 import InfoBox from '../components/InfoBox'
 import Header from '../components/Header'
 import Submit from '../components/Submit'
-import PostList, {
-  ALL_POSTS_QUERY,
-  GET_CANDIDATE,
-  allPostsQueryVars,
-} from '../components/PostList'
+import { DarkModeSwitch } from '../components/DarkModeSwitch'
+import { ALL_MISSIONS_QUERY } from '../components/PostList'
 import { initializeApollo, addApolloState } from '../lib/apolloClient'
+import { Container } from '../components/Container'
 
 const IndexPage = () => (
-  <App>
+  <Container height="100vh">
     <Header />
     <InfoBox>ℹ️ This page shows how to use SSG with Apollo.</InfoBox>
+    <DarkModeSwitch />
     <Submit />
-    {/* <PostList /> */}
-  </App>
+  </Container>
 )
 
 export async function getStaticProps() {
   const apolloClient = initializeApollo()
 
   await apolloClient.query({
-    query: GET_CANDIDATE,
-    // variables: allPostsQueryVars,
+    query: ALL_MISSIONS_QUERY,
   })
 
   return addApolloState(apolloClient, {
