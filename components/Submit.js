cdb604a8ea4d40ba28b9bcdd0adb72afcbfc625a
gaoussou <gaoussoukone@protonmail.com>
2022-01-27 01:08:05 +0100

    upload

--- components/Submit.js
+++ components/Submit.js
@@ -1,20 +1,20 @@
-import {useState} from "react";
+import { useState, useCallback } from "react";
 import { gql, useMutation } from '@apollo/client'
 
 const CREATE_CANDIDATE_MUTATION = gql`
   mutation createCandidat(
     $name: String,
     $lastName: String,
-    $cv: FileFieldInput!,
+    $cv: FileFieldInput,
     $availableFrom: DateTime,
     $availableTo: DateTime
   ) {
     createCandidat(data: {
-      name: $name,
-      lastName: $lastName,
-      cv: $cv,
-      availableFrom: $availableFrom,
-      availableTo: $availableTo
+        name: $name,
+        lastName: $lastName,
+        cv: $cv,
+        availableFrom: $availableFrom,
+        availableTo: $availableTo
       }
     ) {
       name
@@ -27,7 +27,7 @@ export default function Submit() {
   const [file, setFile] = useState(null)
   const [createCandidat, { loading }] = useMutation(CREATE_CANDIDATE_MUTATION)
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault()
     const form = event.target
     const formData = new window.FormData(form)
@@ -35,23 +35,19 @@ export default function Submit() {
     const lastName = formData.get('lastName')
     const availableFrom = new Date(formData.get('availableFrom')).toISOString()
     const availableTo = new Date(formData.get('availableTo')).toISOString()
-    const cv = formData.get('cv')
-    // setFile(cv)
+    form.reset()
 
     createCandidat({
-      variables: {
-        name,
-        lastName,
-        availableFrom,
-        availableTo,
-        cv: {
-          upload: file,
-          // ref:file 
-        }
-      },
+      name,
+      lastName,
+      availableFrom,
+      availableTo,
+      cv: {
+        upload: file,
+      }
     })
 
-    form.reset()
+
   }
 
   return (
@@ -61,7 +57,7 @@ export default function Submit() {
       <input placeholder="lastName" name="lastName" type="text" required />
       <input placeholder="cv" name="cv" type="file" required onChange={(event) => {
         setFile(event.target.files[0])
-      }}/>
+      }} />
       <input placeholder="debut de disponibilite" name="availableFrom" type="date" required />
       <input placeholder="fin de disponibilite" name="availableTo" type="date" required />
       <button type="submit" disabled={loading}>
